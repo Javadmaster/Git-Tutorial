01b8542812c408a4c414020d21a7097e32224105
Javadmaster <javadmaster70@gmail.com>
2022-06-27 14:16:13 +0100

    youtube commit

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("hello")
-        print("Experimental")
+        print("YouTube")
     }
 
 }
